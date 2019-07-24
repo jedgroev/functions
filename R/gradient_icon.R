095a22f45428b80173495091735673480eb6641b
jedgroev <degroevejohannes@gmail.com>
2019-07-24 13:59:13 +0200

    update icon and gradient_icon function

--- R/gradient_icon.R
+++ R/gradient_icon.R
@@ -9,17 +9,27 @@
 #' @param title title of the gradient scale 
 #' @param dist distance from x and y top corner
 #' @param tcol color of labels and title
+#' @param filename the name of the legend_icon when saving as a png, if NULL the png is not exported
+#' @param ratio height-width ratio 
 
 #' @keywords gradient, plot
 #' @export
 
 #' @examples 
-#' export(gradient_icon(col=colorRampPalette(c('grey90','grey20'))(100), labels = c(500,1000), tdist=0.05,bars=10,title=NULL),res2=30,ratio=0.8,'gradient_icon',type='png') 
+#' gradient_icon(col=colorRampPalette(c('grey90','grey20'))(100), labels = c(500,1000), tdist=0.2,bars=10,title=NULL,filename='gradient_icon',res2=30,ratio=0.5) 
 
-gradient_icon <- function(col=colorRampPalette(c('grey90','grey20'))(100), labels = c(0,1), tdist=0.01,bars=10,title=NULL, dist=c(0.15,0.15), tcol='white'){
-par(bg=NA, mar=c(0,0,1,0),oma=c(0,0,1,0), cex=2.8)
-x <- 0:1
-y <- 0:1
-plot(x,y,type='n',axes=FALSE,ann=FALSE) 
-gradient(x,y, col, len=c(0.7,0.8), labels=labels, bars=bars, title=title, tdist=tdist,dist=dist,tcol=tcol) 
+gradient_icon <- function(col=colorRampPalette(c('grey90','grey20'))(100), labels = c(0,1), tdist=0.2,bars=10,title=NULL,dist=c(0.40,0.20),tcol='white',filename=NULL,ratio=0.5,res2=30){
+
+ff <- function(){
+	par(bg=NA, mar=c(0,0,1,0),oma=c(0,0,1,0), cex=2.8)
+	x <- 0:1
+	y <- 0:1
+	plot(x,y,type='n',axes=FALSE,ann=FALSE) 
+	gradient(x,y, col, len=c(0.7,0.8), labels=labels, bars=bars, title=title, tdist=tdist,dist=dist,tcol=tcol) 
+	}
+ff()
+
+if(is.null(filename) == FALSE){export(func=ff(),filename,ratio=ratio,res2=res2,type='png')}
+
+
 }
--- R/icon.R
+++ R/icon.R
@@ -5,15 +5,21 @@
 #' @param pch symbol to use
 #' @param col color border
 #' @param bg color background
+#' @param filename the name of the icon when saving as a png, if NULL the png is not exported
 
 #' @keywords export, icon, png
 #' @export
 
 #' @examples 
 #' # generate and export an icon
-#' export(func=icon(pch=21, col='black',bg='white'),'icon',type='png') 
+#' icon(pch=21, col='black',bg='white',filename='icon')
 
-icon <- function(pch=24,col='black',bg='white'){
-par(bg=NA)
-plot(1,1,axes=FALSE,ann=FALSE, cex =40, col=col,bg=bg,pch=pch)
-} 
+icon <- function(pch=24,col='black',bg='white',filename=NULL){
+
+	ff <- function(){
+		par(bg=NA)
+		plot(1,1,axes=FALSE,ann=FALSE, cex =40, col=col,bg=bg,pch=pch)
+		}
+	ff()
+if(is.null(filename) == FALSE){export(func=ff(),filename,type='png')}
+}
